423e25a2f9c49e4e6b064c6f9935ce3a097db322
zhihui <zhihui.jiang@sumscope.com>
2015-01-29 00:58:11 -0800

    Finish web page crawl and html parse using libcurl and htmlcxx library

--- include/spider.h
+++ include/spider.h
@@ -9,6 +9,9 @@
 #define MDSERVER_SPIDER_H_
 
 #include <string> 
+#include <vector> 
+#include "html/ParserDom.h"
+
 
 class spider{
 public:
@@ -22,6 +25,10 @@ private:
 	//Read the default configuration file and initialize the url and db table names
 	bool init(std::string&); 
 	
+	//parse a html table from an htmlcxx iterator pointing to table tree root,return a two dimensional vector corresponding to the table rows and colomuns
+	std::vector<std::vector<std::string> > parse_html_table(tree<htmlcxx::HTML::Node> &,  tree<htmlcxx::HTML::Node>::iterator &table_root); 		
+	
+	//this is a callback function used by curl to receive web page data
 	static size_t write_html(void * buffer, size_t size,size_t blocks, std::string * html_p);
 		
 private:
--- src/spider.cc
+++ src/spider.cc
@@ -16,27 +16,30 @@
 #include "util.h"
 #include "html/ParserDom.h"
 
-spider::spider(std::string cfg_file){
+using namespace htmlcxx; 
+using namespace std; 
+
+spider::spider(string cfg_file){
 	init(cfg_file);
 }
 
 /*
 * Read default configurations from a setting file spider.ini using boost library program_options
 */
-bool spider::init(std::string & config_file){
+bool spider::init(string & config_file){
 	namespace po = boost::program_options; 
-	DEBUG("setup...");
+	//DEBUG("setup...");
 
 	//setup options
 	po::options_description desc("Options");
 	desc.add_options()
-		("web.url",po::value<std::string> (&url))
-		("db.db_name",po::value<std::string> (&db_name))
-		("db.table_name",po::value<std::string> (&table_name));	
+		("web.url",po::value<string> (&url))
+		("db.db_name",po::value<string> (&db_name))
+		("db.table_name",po::value<string> (&table_name));	
 
 	//load configuration file 
 	po::variables_map vm;
-	std::ifstream setting_file(config_file.c_str()); 
+	ifstream setting_file(config_file.c_str()); 
 	if(!setting_file)
 		return false;
 	po::store(po::parse_config_file(setting_file,desc),vm);
@@ -57,7 +60,7 @@ bool spider::crawl(){
 	CURL *curl = curl_easy_init();
 	if(NULL == curl)
 		return false;
-	//DEBUG(url);
+	////DEBUG(url);
 
 	//setup curl options
 	curl_easy_setopt(curl,CURLOPT_URL,url.c_str());
@@ -77,7 +80,7 @@ bool spider::crawl(){
 /*
 Callback function used by curl to write data to html_doc
 */
-size_t spider::write_html(void * buffer, size_t size, size_t blocks, std::string  * html_p){
+size_t spider::write_html(void * buffer, size_t size, size_t blocks, string  * html_p){
 	if(html_p == NULL){
 		ERROR("html_p NULL");
 		return 1;
@@ -87,29 +90,56 @@ size_t spider::write_html(void * buffer, size_t size, size_t blocks, std::string
 }
 
 /*
-Parse the content crawled by crawl() function and store the parsed values in a map
+parse a table in html document
+@param1: dom, the html document 
+@param2: table_root, the root iterator pointing to the table in html doc
+@return: return two dimensional vector corresponding to table
+*/
+vector<vector<string> > spider::parse_html_table(tree<HTML::Node> & dom,  tree<HTML::Node>::iterator &table_root){
+	vector<vector<string> > table; 
+	tree<HTML::Node>::sibling_iterator row_it = dom.begin(table_root); 
+	//iterate through each row in the table
+	while(row_it != dom.end(table_root)){
+		if(row_it->tagName() != "tr"){
+			++row_it;
+			continue;
+		}
+		tree<HTML::Node>::sibling_iterator col_it = dom.begin(row_it);
+		vector<string> row; 
+		//iterate through each column
+		while(col_it != dom.end(row_it)){
+			if(col_it->tagName() != "td"){
+				++col_it;
+				continue;
+			}
+			tree<HTML::Node>::sibling_iterator content = dom.child(col_it,0); 
+			if(content != NULL){
+				string data = content->text();
+				boost::trim(data);
+				row.push_back(data);	
+			}
+			++col_it;
+		}
+		table.push_back(row);
+		++row_it;
+	}
+	return table;
+}
+
+/*
+Parse the content crawled by crawl() function and store the parsed values in a two-dimensional vector 
 */
 bool spider::parse(){
-	using namespace htmlcxx; 
 	HTML::ParserDom parser; 
 	tree<HTML::Node> dom = parser.parseTree(html_doc); 	
 	tree<HTML::Node>::iterator it = dom.begin(); 
-	std::map<std::string,std::string> values;
+	vector<vector<string> > table;
 	for(; it != dom.end(); it++){
-		if(!it->isTag() && !it->isComment()){
-			std::string txt = it->text();
-			boost::trim(txt);		
-			if(txt.size() == 0) continue;	
-			if(boost::contains(txt,"CNY")){
-				++it; 
-				std::cout << it->text();
-				values[txt] = it->text();	
-			}
+		if(it->isTag() && it->tagName() == "table"){
+			table = parse_html_table(dom,it);
+			break;
 		}
 	}
-	std::cout << std::endl;
-	for(std::map<std::string,std::string>::iterator it = values.begin(); it != values.end(); ++it){
-		std::cout << it->first << ":" << it->second << std::endl;
-	}
-	return true; 
+	return table.size() > 0; 
 }
+
